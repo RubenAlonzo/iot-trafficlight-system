b76509711219a624e48a1d338409beddfcac5f71
RubenAlonzo <20186966@itla.edu.do>
2021-08-21 16:46:33 -0400

    Refactor and configure MQTT for TrafficLight

--- src/trafficlight/mqttClient.js
+++
@@ -1,9 +0,0 @@
-var client = mqtt.connect('ws://localhost:8888') // you add a ws:// url here
-
-client.on('connect', ()=>{
-  client.subscribe("counter/color")
-})
-
-client.on('message', (topic, message)=>{
-  console.log(topic + ": " + message)
-})
--- src/trafficlight/static/js/index.js
+++ src/trafficlight/static/js/index.js
@@ -6,5 +6,8 @@ const setActiveLight = (lightIndex = 0) => {
         else light.style.opacity = 0.2;
     })
 };
-     
 
+// TODO: Implement the https://www.npmjs.com/package/web-traffic-light library for the traffic light
+
+// TODO: The trafficlight should adjust its light according to the counter topics. 
+// A algorithm to inffer trafficlight state from counter state should be implemented 
