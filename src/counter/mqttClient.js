fef8ed27967cd8faf8ed6d055106b50b05684e49
RubenAlonzo <20186966@itla.edu.do>
2021-08-21 16:12:15 -0400

    Refactor and configure MQTT for counter

--- src/counter/mqttClient.js
+++
@@ -1,8 +0,0 @@
-var client = mqtt.connect("ws://localhost:8888") // you add a ws:// url here
-
-client.on('connect', ()=>{
-  setInterval(()=>{
-    client.publish("counter/color", "current counter color")
-    console.log('Message sent!')
-  }, 1000)
-})
--- src/counter/server.js
+++ src/counter/server.js
@@ -1,31 +1,29 @@
 const express = require('express');
 const app = express();
-app.set('port', process.env.PORT || 3001); // Set to port 8080 in production
 
+// DEFINE APPLICATION LISTENER
+app.set('port', process.env.PORT || 3001); // Set to port 8080 in production
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
 });
 
+//CONFIGURE STATIC RESOURCES https://expressjs.com/en/4x/api.html#express.static
+let options = {
+  dotfiles: "ignore",
+  etag: true,
+  extensions: ["htm", "html"],
+  index: false,
+  maxAge: "7d",
+  redirect: false,
+  setHeaders: function(res, path, stat) {
+    //add this header to all static responses
+    res.set("x-timestamp", Date.now());
+  }
+};
+
+app.use(express.static("static", options));
+
+// DEFINE ENDPOINTS
 app.get('/', (req, res) => {
-  res.sendFile(__dirname +'/index.html');
-})
-
-app.get('/styles.css', (req, res) => {
-  res.sendFile(__dirname +'/styles.css');
-})
-
-app.get('/index.js', (req, res) => {
-  res.sendFile(__dirname +'/index.js');
-})
-
-app.get('/browserMqtt.js', (req, res) => {
-  res.sendFile(__dirname +'/browserMqtt.js');
-})
-
-app.get('/mqttClient.js', (req, res) => {
-  res.sendFile(__dirname +'/mqttClient.js');
-})
-
-app.get('/fonts/digital-7.ttf', (req, res) => {
-  res.sendFile(__dirname +'/fonts/digital-7.ttf');
+  res.sendFile(__dirname +'/static/html/index.html');
 })
