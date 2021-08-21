94dd07b239ce72588f95259d9190dd701ca82859
RubenAlonzo <20186966@itla.edu.do>
2021-08-21 17:39:13 -0400

    Add Web App structure and subscriptions

---
+++ src/webapp/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// DEFINE APPLICATION LISTENER
+app.set('port', process.env.PORT || 3004); // Set to port 8080 in production
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
+
+//CONFIGURE STATIC RESOURCES https://expressjs.com/en/4x/api.html#express.static
+let options = {
+  dotfiles: "ignore",
+  etag: true,
+  extensions: ["htm", "html"],
+  index: false,
+  maxAge: "7d",
+  redirect: false,
+  setHeaders: function(res, path, stat) {
+    //add this header to all static responses
+    res.set("x-timestamp", Date.now());
+  }
+};
+
+app.use(express.static("static", options));
+
+// DEFINE ENDPOINTS
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +'/static/html/index.html');
+})
---
+++ src/webapp/static/js/mqttClient.js
@@ -0,0 +1,12 @@
+var client = mqtt.connect('ws://localhost:8080') // you add a ws:// url here
+
+// When connecting to the mqtt broker, it subscribes to the counter events
+client.on('connect', ()=>{
+  client.subscribe("button/click")
+  client.subscribe("smartphone/click")
+})
+
+// This is how mqtt listen message subscriptions. Modify actions according to topic
+client.on('message', (topic, message)=>{
+  console.log(topic + ": " + message)
+})
