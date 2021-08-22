1502104ac36d26349565b0c5c1306b9482101492
Jorge <jorge.tamariz@aloe.com.do>
2021-08-22 15:19:17 -0400

    change traffic light implementation on its device

--- src/trafficlight/static/js/index.js
+++ src/trafficlight/static/js/index.js
@@ -1,13 +1,11 @@
-const setActiveLight = (lightIndex = 0) => {
-    let lights = document.querySelectorAll('.traffic-light > span')
-    lights.forEach((light, index) => {
-        if (lightIndex === index) 
-            light.style.opacity = 1;
-        else light.style.opacity = 0.2;
-    })
+const setActiveLight = (lightColor = "green") => {
+    let trafficLight = document.querySelector('traffic-light')
+    trafficLight.color = lightColor;
 };
 
+
 // TODO: Implement the https://www.npmjs.com/package/web-traffic-light library for the traffic light
+
 
 // TODO: The trafficlight should adjust its light according to the counter topics. 
 // A algorithm to inffer trafficlight state from counter state should be implemented 
