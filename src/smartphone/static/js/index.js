2700d09290ce74f43907270c20168be787660a53
RubenAlonzo <20186966@itla.edu.do>
2021-08-21 23:52:09 -0400

    Add mqtt client logic to smartphone device

--- src/smartphone/static/js/index.js
+++ src/smartphone/static/js/index.js
@@ -11,9 +11,9 @@ const shakePhone = () => {
 
 beginBTN.addEventListener("click", () => {
     shakePhone();
-    treatDeviceEvent("cross-click")
+    treatDeviceEvent("cross-click") //Simulate a real event captured by the IoT device
 });
 
 cancelBTN.addEventListener("click", () => {
-    treatDeviceEvent("cancel-click")
+    treatDeviceEvent("cancel-click") //Simulate a real event captured by the IoT device
 })
