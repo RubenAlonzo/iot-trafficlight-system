0dd09e34295647e88dd293b4f0de7f3e7b06aa36
Jorge <jorge.tamariz@aloe.com.do>
2021-08-22 20:16:25 -0400

    create web app component and its methods

--- src/webapp/static/js/index.js
+++ src/webapp/static/js/index.js
@@ -1,16 +1,80 @@
+const getRandomLocation = () => {
+    const locations = [
+        "Dooley Skyway",
+        "Rau Hills",
+        "Ernser Dale",
+        "Emilie Highway",
+        "Sipes Greens",
+    ];
+    return locations[Math.floor(Math.random() * locations.length)];
+};
+
 const generateGuid = () => {
     return window.URL.createObjectURL(new Blob([])).split("/").pop();
 };
 
-const addTableRow = (steps) => {
+const recalculateTotalSteps = () => {
+    const localStorageKey = "totalSteps";
+    const totalSteps = localStorage.getItem(localStorageKey) ?? 0;
+    const newTotalSteps = parseInt(totalSteps) + 10
+    localStorage.setItem(localStorageKey, newTotalSteps);
+    return newTotalSteps
+};
+
+const getTotalStepsElement = () => {
+    return document
+        .getElementById("totalSteps")
+};
+
+const generateRowData = () => {
+    return {
+        id: generateGuid(),
+        location: getRandomLocation(),
+        hour: new Date().toLocaleTimeString(),
+    };
+};
+
+const getLocalData = () => {
+    const localData = localStorage.getItem("crossHistory") ?? JSON.stringify([]);
+    return JSON.parse(localData);
+};
+
+const addNewRow = (newRow) => {
+    const localData = getLocalData();
+    localStorage.setItem(
+        "crossHistory",
+        JSON.stringify([...localData, newRow])
+    );
+};
+
+const addTableRow = ({ id, location, hour }) => {
     const row = document.createElement("tr");
     const idRegister = document.createElement("td");
-    idRegister.textContent = generateGuid();
-    const stepsRegister = document.createElement("td");
-    stepsRegister.textContent = steps;
+    idRegister.textContent = id;
+    const placeRegister = document.createElement("td");
+    placeRegister.textContent = location;
+    const hourRegister = document.createElement("td");
+    hourRegister.textContent = hour;
     row.appendChild(idRegister);
-    row.appendChild(stepsRegister);
+    row.appendChild(placeRegister);
+    row.appendChild(hourRegister);
 
     document.querySelector("tbody").appendChild(row);
 };
 
+const renderTableRows = () => {
+    const tableData = getLocalData();
+    tableData.forEach((row) => {
+        addTableRow(row);
+    });
+};
+
+const saveNew = () => {
+    const newData = generateRowData();
+    addNewRow(newData);
+    addTableRow(newData);
+    getTotalStepsElement().textContent = `Total steps: ${recalculateTotalSteps()}`;
+}
+
+renderTableRows();
+saveNew()
--- src/webapp/static/js/mqttClient.js
+++ src/webapp/static/js/mqttClient.js
@@ -1,12 +1,20 @@
-var client = mqtt.connect('ws://localhost:8080') // you add a ws:// url here
+const buttonTopic = "button/click";
+const smartphoneTopic = "smartphone/click";
+var client = mqtt.connect("ws://localhost:8080"); // you add a ws:// url here
 
 // When connecting to the mqtt broker, it subscribes to the counter events
-client.on('connect', ()=>{
-  client.subscribe("button/click")
-  client.subscribe("smartphone/click")
-})
+client.on("connect", () => {
+    client.subscribe("button/click");
+    client.subscribe("smartphone/click");
+});
 
 // This is how mqtt listen message subscriptions. Modify actions according to topic
-client.on('message', (topic, message)=>{
-  console.log(topic + ": " + message)
-})
+client.on("message", (topic, message) => {
+    console.log(topic + ": " + message);
+
+    if (topic == smartphoneTopic) {
+        if (message == "cross") {
+            saveNew();
+        }
+    }
+});
